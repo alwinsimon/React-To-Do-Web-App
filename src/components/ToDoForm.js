16f572cf0946319d2d0c44dc01e5058c57ae3d93
Alwin Simon <003alwin@gmail.com>
2023-08-07 13:11:21 +0530

    Configured submission form

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const ToDoForm = () => {
+const ToDoForm = () => {
+
+  const toDoFormPlaceholder = "Jot in your next big thing here...";
+
+  const [formValue, setFormValue] = useState("");
+
+  const handleToDoFormSubmission = e => {
+    
+    e.preventDefault();
+    
+    console.log(formValue);
+    
+  }
+
   return (
-    <div>ToDoForm</div>
+
+    <form className='ToDoForm' onSubmit={handleToDoFormSubmission}>
+
+      <input type='text' className='ToDo-form-input' placeholder = {toDoFormPlaceholder} onChange={(e)=> setFormValue(e.target.value)} />
+
+      <button type='submit' className='todo-form-submit-btn'> Add Task </button>
+
+    </form>
+
   )
 }
+
+
+export default ToDoForm;
