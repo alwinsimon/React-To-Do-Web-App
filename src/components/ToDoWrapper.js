b23c77e0b8cffe61ec11d1f2f2c37eef053995a9
Alwin Simon <003alwin@gmail.com>
2023-08-07 15:03:58 +0530

    Added method to store the form submission to an array

--- src/components/ToDoWrapper.js
+++ src/components/ToDoWrapper.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ToDoForm from './ToDoForm';
+import {v4 as uuidv4} from 'uuid';
+uuidv4();
 
 const ToDoWrapper = () => {
+
+  const [toDoList, setToDoList] = useState([]);
+
+  const addToDo = (newFormSubmission) => {
+
+    const newDataToInsert = {
+      
+      id: uuidv4(),
+      task: newFormSubmission,
+      completed: false,
+      isEditing: false
+
+    }
+
+    // Update the existing to-do list array with the new data received from the form submission
+    setToDoList([...toDoList, newDataToInsert]);
+
+  }
 
   return (
 
     <div className='ToDoWrapper'>
 
-      <ToDoForm/>
+      <ToDoForm addToDo={addToDo} />
 
     </div>
 
